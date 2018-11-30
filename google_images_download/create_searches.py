79aeb6e8342794c2c58b7eb6a8848821fde1292f
Axel Sly <axelsly@alumni.stanford.edu>
2018-11-30 15:47:21 -0800

    added search config file creator from template

---
+++ google_images_download/create_searches.py
@@ -0,0 +1,55 @@
+import json
+import argparse
+import re
+from jinja2 import Environment, FileSystemLoader
+import os
+
+
+
+def generate_arg_parser():
+    desc = u'{0} [Args] [Options]\nDetailed options -h or --help'.format(__file__)
+    parser = argparse.ArgumentParser(description=desc)
+
+    parser.add_argument(
+        '--output-dir',
+        type=str,
+        default='./',
+        help='Output directory of search json'
+    )
+
+    return parser
+
+
+def create_search_json(output_dir):
+    # Query user for image types for this dataset
+    image_search_type = input("Image search type:\n")
+
+    jinja_env = Environment(loader=FileSystemLoader(searchpath="./"))
+    template = jinja_env.get_template('search.json.template')
+
+    # with open("search.json.template", 'r') as json_template:
+    #     template_json_format_string = json_template.read()
+    # print (template_json_format_string)
+
+    while(True):
+        # Query user for search
+        image_search_keywords = input("Image search keywords:\n")
+        if image_search_keywords == "":
+            break
+
+        # Fill out template
+        formatted_json_string = template.render(keywords=image_search_keywords, type=image_search_type)
+        output_filename = "{0}.json".format(re.sub('\s+', '-', image_search_keywords))
+        output_filepath = os.path.join(output_dir, output_filename)
+
+        with open(output_filepath, 'w') as outfile:
+            outfile.write(formatted_json_string)
+
+
+if __name__ == '__main__':
+    parser = generate_arg_parser()
+    args = parser.parse_args()
+    create_search_json(args.output_dir)
+
+
+
